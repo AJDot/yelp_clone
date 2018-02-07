d8c52890398a669e0daa6eb0e00c1ff62b22b6d5
Alex Henegar <alex.henegar19@gmail.com>
2018-02-07 16:32:16 -0500

    create validation tests for business model

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -1,4 +1,13 @@
 describe Business do
-  # it { should validate_presence_of(:name) }
-  # it { should validate_presence_of(:description) }
+  it 'must have a name' do
+    new_user = Fabricate.build(:business, name: nil)
+    new_user.save
+    expect(Business.count).to eq(0)
+  end
+
+  it 'must have a description' do
+    new_user = Fabricate.build(:business, description: nil)
+    new_user.save
+    expect(Business.count).to eq(0)
+  end
 end
