f3153788922ad35eab877ce45b032cff4821c755
Alex Henegar <alex.henegar19@gmail.com>
2018-02-09 19:51:19 -0500

    create Rating class method average_by_business

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,10 @@ class Review < ActiveRecord::Base
   belongs_to :business
 
   validates_presence_of :rating, :body
+
+  def self.average_by_business(business)
+    reviews = where business: business
+    return 0 if reviews.empty?
+    (reviews.map(&:rating).sum / reviews.count.to_f).round(1)
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -22,4 +22,29 @@ describe Review do
     review.save
     expect(User.count).to eq(0)
   end
+
+  describe 'average_by_business' do
+    it 'returns the average rating for the business' do
+      business = Fabricate(:business)
+      Fabricate(:review, rating: 2, business: business)
+      Fabricate(:review, rating: 4, business: business)
+      average = Review.average_by_business(business)
+      expect(average).to eq(3)
+    end
+
+    it 'returns the average rating for the business as a decimal with 1 precision' do
+      business = Fabricate(:business)
+      Fabricate(:review, rating: 3, business: business)
+      Fabricate(:review, rating: 4, business: business)
+      Fabricate(:review, rating: 4, business: business)
+      average = Review.average_by_business(business)
+      expect(average).to eq(3.7)
+    end
+
+    it 'returns 0 if there are no ratings' do
+      business = Fabricate(:business)
+      average = Review.average_by_business(business)
+      expect(average).to eq(0)
+    end
+  end
 end
